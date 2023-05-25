f8be47f54cff53f6b66bed61c7c39009aabf3948
Olatunde Mosobalaje <olatunde.mosobalaje@covenantuniversity.edu.ng>
2023-05-25 15:13:10 +0100

    Create function_rate.py

---
+++ function_rate.py
@@ -0,0 +1,6 @@
+# ttowG
+
+def darcy_rate(perm, area, p_in, p_out, visc, interval, cf = 0.001127):
+    rate = (cf*perm*area*(p_in - p_out))/(visc*interval)
+    rate = round(rate, 4)
+    return rate
