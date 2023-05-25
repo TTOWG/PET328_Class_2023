10c6dfc000fc35dcc188f092f7f4c8a0855c1504
Olatunde Mosobalaje <olatunde.mosobalaje@covenantuniversity.edu.ng>
2023-05-25 14:32:48 +0100

    Update function_stoiip_calc.py

--- function_stoiip_calc.py
+++ function_stoiip_calc.py
@@ -3,4 +3,5 @@
 ########  A function to compute Stock Tank Oil Initially In-Place (STOIIP), N ########
 def stoiip(area, thickness, poro, sw, boi):
     N = (7758*area*thickness*poro*(1-sw))/boi
-    return round(N, 2)
+    N = round(N, 2)
+    return N
